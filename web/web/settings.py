291f1ae3a184484f1e60a59ec66665431639ca32
Clark Van Oyen <clark@workstation>
2019-05-31 15:01:16 -0700

    use pg

--- web/web/settings.py
+++ web/web/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'HOST': 'db',
+        'PASSWORD': 'postgres',
+        'USERNAME': 'postgres',
+        'PORT': 5432,
     }
 }
 
